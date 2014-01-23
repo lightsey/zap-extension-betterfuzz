e9ed165917490e1dfaf834df2d6fe8549a408fe5
John Lightsey <jd@cpanel.net>
2014-01-23 12:39:48 -0600

    Make quickbuild.sh slightly smarter about detecting that it's not on my personal machine.

--- quickbuild.sh
+++ quickbuild.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-ant -f build/build.xml -lib lib clean && ant -f build/build.xml -lib lib && cp build/zap-exts/betterfuzz-*.zap ~/bin/ZAP_2.2.2/plugin/
+# Change to match real install directory for plugins
+ZAP_INSTALL_PLUGIN_DIR=~/bin/ZAP_2.2.2/plugin/
+
+if [ -e $ZAP_INSTALL_PLUGIN_DIR ]; then
+    ant -f build/build.xml -lib lib clean && ant -f build/build.xml -lib lib && cp build/zap-exts/betterfuzz-*.zap $ZAP_INSALL_PLUGIN_DIR
+else
+    echo "You must configure ZAP_INSTALL_PLUGIN_DIR correctly for quickbuild to function"
+fi
